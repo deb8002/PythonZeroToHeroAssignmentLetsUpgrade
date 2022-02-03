52154c4349dcf3e59a78b8cb8794eddde935b55f
deb8002 <98836092+deb8002@users.noreply.github.com>
2022-02-03 17:54:45 +0530

    Day 3 assignment submitted

---
+++ PythonZeroToHeroDay3.py
@@ -0,0 +1,4 @@
+variable={"First":5, "Second":80, "Third":20, "Fourth":5120, "Fifth":1280, "Sixth":320}
+maximum= max(variable, key = variable.get)
+maximumvalue= max(variable.values())
+print("From the dictionary 'variable' the key whose value is maximum is",maximum,"and the value of",maximum,"is",maximumvalue)
